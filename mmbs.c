9da3101802301c6101f33bf90275d98b53c113c7
Wojciech Meller <wojmel97@gmail.com>
2017-12-22 01:36:03 +0100

    Wstepny szkic

--- mmbs.c
+++ mmbs.c
@@ -1,4 +1,6 @@
-
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdint.h>
 
 #define _nbit(i)             (i & 0x1f)
 #define _ibits(ptr,i)        (((uint32_t*)ptr)[ i >> 5 ])
@@ -12,10 +14,20 @@ bool MM_INIT = false;
 size_t PAGE_SIZE = sysconf(_SC_PAGESIZE);
 size_t num_pages = 0;
 void *page_tree = NULL;
-size_t page_adrs_size = 0;
 size_t page_adrs_cap = 0;
 void **page_adrs = NULL;
-size_t page_adrs_start_size = sizeof(void*)
+
+/*
+   drzewo strony:
+      * wymaga 1 slowo na 16 slow obslugiwanych
+      * ma strukture:
+         MLP
+         M - bit mowiacy czy L i P sa polaczone
+         L i P - lewe i prawe drzewo-dziecko
+      * drzewa-liscie skladaja sie z 3 bitow
+         podobnie MLP ale
+         L i P to bity mowiace o zajeciu danego slowa
+*/
 
 void *malloc(size_t size){
    if( size == 0 ) return NULL;
@@ -34,6 +46,22 @@ void *malloc(size_t size){
    // if failed create new page
 }
 
+void *realloc(void *ptr, size_t size){
+   if( ptr == NULL ) return NULL;
+   if( size == 0 ) { free(ptr); return NULL: }
+   if( size > (PAGE_SIZE >> 1) ){
+      // free malloc cpmem
+   }
+   // usun z drzewa
+   // find slot
+   // jezeli inny adres cpymem
+}
+
+void free(void *ptr){
+   // usun z drzewa
+   // jesli w pamieci tylko drzewo i tablica stron mm_cleanup
+}
+
 static inline void to_slot_size(size_t *size){
    if( ! (*size >> 2) ){ *size = 4; return; }
    // https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
@@ -48,26 +76,50 @@ static inline void to_slot_size(size_t *size){
 }
 
 void mm_startup(){
+   /*
+      tworzy pierwsza strone
+      na jej poczatku inicjuje liste drzew stron
+      tworzy liste stron 
+   */
    void *page = mmap(0, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+
+   page_tree = page;
+   setbit(page_tree, 0);
+   find_slot_page(page_tree, page, PAGE_SIZE >> 6);
+
+   page_adrs = find_slot_page(page_tree, page, sizeof(void*));
+   page_adrs_cap = 1;
+   page_adrs[0] = page;
    ++num_pages;
-   init_page_tree(page, PAGE_SIZE >> 2);
+
+   MM_INIT = true;
 }
 
 void mm_cleanup(){
 
+   // to-do
+
+   MM_INIT = false;
 }
 
-void init_page_tree(void *ptr, size_t size){
-   // assert size > 2 and Ek 2^k = size
-   page_tree = ptr;
-   setbit(page_tree, 0);
-   find_slot(page_tree, ptr, size >> 4);
-}
 
-void *find_slot(void *tree, void *page, size_t k){
-
+void *find_slot_page(void *tree, void *page, size_t k){
+   /*
+      Znajduje slot dlugosci k-slow w danej stronie
+      aktualizuje drzewo strony
+   args
+      tree - drzewo danej strony
+      page - dana strona
+      k    - dlugosc szukanego slotu w slowach, potega 2
+   return
+      ptr - adress do wolnego slotu o dlugosci k-slow, NULL gdy taki slot nie istnieje
+   */
+   size_t cur_len = PAGE_SIZE << 2;
    size_t offset = 0;
-
+   size_t i = 0;
+   
+   // to-do
+   // obliczenie offset i modyfikacja drzewa
 
    return page + offset;
 }
